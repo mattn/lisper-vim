6f00dbffb9b47ba52b016c68ce7ff46b90fb00f4
mattn <mattn.jp@gmail.com>
2011-11-21 14:38:03 +0900

    don't use recursible call.

--- example/fizzbuzz.lisp
+++ example/fizzbuzz.lisp
@@ -1,16 +1,14 @@
 (begin
-  (define FizzBuzz (lambda (n)
-    (if (> n 1)
-      (begin
-        (vim-echo (FizzBuzz (- n 1)))
-        (if (= (mod n 15) 0)
-          "FizzBuzz" (if (= (mod n 5) 0)
-            "Buzz" (if (= (mod n 3) 0)
-              "Fizz" n))
-        )
-      ) 1))
+  (set! i 0)
+  (loop
+    (if (= i 100) (return i))
+    (set! i (+ i 1))
+    (vim-echo (if (= (mod i 15) 0)
+      "FizzBuzz" (if (= (mod i 5) 0)
+        "Buzz" (if (= (mod i 3) 0)
+          "Fizz" i))
+    ))
   )
-  (FizzBuzz 22)
 )
 
 ;; vim: set ft=lisp et:
