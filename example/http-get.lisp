0e0f01bec8ac6e4600ff28f393d343298812a201
mattn <mattn.jp@gmail.com>
2012-04-03 18:23:14 +0900

    use namespace 'webapi'.

--- example/http-get.lisp
+++ example/http-get.lisp
@@ -1,12 +1,12 @@
 (begin
   (define http-get
-    (lambda (url) (vim-eval "http#get(a:1).content" url))
+    (lambda (url) (vim-eval "webapi#http#get(a:1).content" url))
   )
   (define from-utf8
     (lambda (str) (vim-call "iconv" str "utf-8" (vim-eval "&encoding")))
   )
   (define html-parse
-    (lambda (html) (vim-eval "html#parse(a:1)" html))
+    (lambda (html) (vim-eval "webapi#html#parse(a:1)" html))
   )
   (define html-to-text
     (lambda (dom) (vim-eval "wwwrenderer#render_dom(a:1)" dom))
